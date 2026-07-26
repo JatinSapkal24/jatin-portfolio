299c2badd886b0348ce3cb4ff8342238ca5ec8b9
JatinSapkal24 <jatinsapkal21@outlook.com>
2026-07-26 15:25:14 +0530

    Revert custom CNAME configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Optimize for custom root domain hosting (jatinsapkal24.com)
+  base: './', // Optimize for GitHub Pages or relative sub-directory hosting
 })
